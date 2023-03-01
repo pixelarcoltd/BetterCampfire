8b367ad37bc956cdeae1244c0c5f30f19776f0ab
Francis Chen <tuangchok.ch@protonmail.com>
2023-03-02 05:36:35 +0700

    initialised plugin

---
+++ src/main/java/com/pixel4r/plugin/enderice/bettercampfire/BetterCampfire.java
@@ -0,0 +1,18 @@
+package com.pixel4r.plugin.enderice.bettercampfire;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterCampfire extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
